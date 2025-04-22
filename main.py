37933f4ae7045c4b0c24369e285109a86367f75e
Talat Fakhri <talatprog@gmail.com>
2025-04-23 04:59:18 +0800

    v5 + requirements.txt

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 # main.py
 
+# from fastapi import FastAPI
+
+# app = FastAPI()
+
+# @app.get("/")
+# def read_root():
+#     return {"message": "Welcome to my FastAPI app!"}
+
+# @app.get("/hello/{name}")
+# def read_item(name: str):
+#     return {"message": f"Hello, {name}!"}
+
+######
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Welcome to my FastAPI app!"}
-
-@app.get("/hello/{name}")
-def read_item(name: str):
-    return {"message": f"Hello, {name}!"}
+    return {"message": "Hello from FastAPI"}
 
