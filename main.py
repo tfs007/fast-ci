4c837b62116ce9fdaff7238a54314479895a54d0
Talat Fakhri <talatprog@gmail.com>
2025-04-23 00:32:43 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# main.py
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to my FastAPI app!"}
+
+@app.get("/hello/{name}")
+def read_item(name: str):
+    return {"message": f"Hello, {name}!"}
+
